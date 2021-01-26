83bad74ac672fbe22e4a1035e79f517331455d34
david guzikowski <guzikowski.david@gmail.com>
2021-01-26 01:23:38 -0500

    Component Part 1

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 import { Navbar, NavbarBrand } from 'reactstrap';
+import Directory from './components/DirectoryComponent';
 import './App.css';
+
 
 class App extends Component {
     render() {
@@ -11,9 +13,11 @@ class App extends Component {
                     <NavbarBrand href="/">NuCamp</NavbarBrand>
                 </div>
                 </Navbar>
+                <Directory />
             </div>
         );
     }
 }
+
 
 export default App;
