f595d1fb46e4076fa75b1880ad7b0b4c168b45bd
david guzikowski <guzikowski.david@gmail.com>
2021-01-30 18:56:02 -0500

    Week 2 Assignment

---
+++ src/components/CampsiteInfoComponent.js
@@ -0,0 +1,70 @@
+import React, { Component } from 'react';
+import { Card, CardImg, CardImgOverlay, CardText, CardBody, CardTitle } from 'reactstrap';
+
+// create campsiteinfo class component
+
+class CampsiteInfo extends Component {
+    constructor(props) {
+        super(props);
+    }
+
+    // render and pass in campsite object
+    renderCampsite(campsite) {
+        return(
+            <div className="col-mid-5 m-1">
+                <Card>
+                    <CardImg top src={campsite.image} alt={campsite.name} />
+                    <CardBody>
+                        <CardTitle>{campsite.name}</CardTitle>
+                        <CardText>{campsite.description}</CardText>
+                    </CardBody>
+                </Card>
+            </div>
+        );
+    }
+
+    renderComments(comments) {
+        if (comments) {
+            return (
+                <div className="col-md-5 m-1">
+                    <h4>Comments</h4>
+                    {comments.map(comment => {
+                            return (
+                                <div>
+                                    <p>{comment.text}</p>
+                                    <p>{new Intl.DateTimeFormat('en-US', { year: 'numeric', month: 'short', day: '2-digit'}).format(new Date(Date.parse(comment.date)))}</p>
+                                </div>
+                            );   
+                        }
+                    )
+                }
+                </div>
+            )
+        }
+
+        return (
+                <div>
+
+                </div>
+        )
+    }
+
+    render () {
+            if (this.props.campsite) {
+                    return (
+                        <div className="row">
+                            {this.renderCampsite(this.props.campsite)}
+                            {this.renderComments(this.props.campsite.comments)}
+                        </div>
+                    );
+            } else {
+                return (
+                    <div>
+
+                    </div>
+                );
+            }
+       }
+}
+
+export default CampsiteInfo;
--- src/components/DirectoryComponent.js
+++ src/components/DirectoryComponent.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import CampsiteInfo from './CampsiteInfoComponent';
 import { Card, CardImg, CardImgOverlay, CardText, CardBody, CardTitle } from 'reactstrap';
 
 class Directory extends Component {
@@ -8,25 +9,25 @@ class Directory extends Component {
             selectedCampsite: null
         };
     }
-
+        // Selecting Item on NuCamp page
         onCampsiteSelect(campsite) {
             this.setState({selectedCampsite: campsite});
         }
 
-        renderSelectedCampsite(campsite) {
-            if (campsite) {
-                return (
-                    <Card>
-                        <CardImg top src={campsite.image} alt={campsite.name} />
-                        <CardBody>
-                            <CardTitle>{campsite.name}</CardTitle>
-                            <CardText>{campsite.description}</CardText>
-                        </CardBody>
-                    </Card>
-                );
-            }
-            return <div />;
-        }
+        // renderSelectedCampsite(campsite) {
+        //    if (campsite) {
+        //        return (
+        //            <Card>
+        //                <CardImg top src={campsite.image} alt={campsite.name} />
+        //                <CardBody>
+        //                    <CardTitle>{campsite.name}</CardTitle>
+        //                    <CardText>{campsite.description}</CardText>
+        //                </CardBody>
+        //            </Card>
+        //        );
+        //    }
+        //    return <div />;
+        // }
 
     render() {
         const directory = this.props.campsites.map(campsite => {
@@ -47,11 +48,7 @@ class Directory extends Component {
                 <div className="row">
                     {directory}
                 </div>
-                <div className="row">
-                    <div className="col-md-5 m-1">
-                        {this.renderSelectedCampsite(this.state.selectedCampsite)}
-                    </div>
-                </div>
+                <CampsiteInfo campsite={this.state.selectedCampsite} />
             </div>
         );
     }
